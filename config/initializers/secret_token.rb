c10b8bbbc02b828ccd15da058dcbdbaaddaac009
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-03-04 19:13:39 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stopponline::Application.config.secret_token = '19406a464602617b210161c1d8f03b330714426021bcb95744b19720910b7332c0de599b0df08f406110e6d8317767b43d6738ed35deaff7cecde944e24875e4'
