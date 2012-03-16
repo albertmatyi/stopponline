1ac65c01dff141e5869264acb9815dd9b371ac1a
Matyas Albert Nagy <albertmatyi@gmail.com>
2012-03-16 11:44:57 +0200

    Added suggestion box (js & css) + ajax callback
    TODO: hide browser suggestion & keyboard navigation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= reuiqre boostrap
-//= require_tree .
+//= require routes
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Stopponline::Application.routes.draw do
 
   resources :routes
+  resources :locations
   
   root to: 'static_pages#home'
   devise_for :users
   resources :users, only: :show
   
   get 'static_pages/contact'
-  match 'contact', to: 'static_pages#contact'
+  match 'contact', to: 'static_pages#contact', as: :contact
+  
+  get 'locations/by_prefix'
+  match 'locations/by_prefix/:prefix', to: 'locations#by_prefix'
 
 end
